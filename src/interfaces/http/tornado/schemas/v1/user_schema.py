d272e4f2019c5885319373dadccfd534afd302b4
Nithin <2319226+gnknithin@users.noreply.github.com>
2023-06-09 15:50:41 +0530

    feat (core) : adding SignInSchema and tests

--- src/interfaces/http/tornado/schemas/v1/user_schema.py
+++ src/interfaces/http/tornado/schemas/v1/user_schema.py
@@ -15,3 +15,14 @@ class SignUpSchema(SQLAlchemySchema):
     full_name = auto_field()
     date_of_birth = auto_field()
     email = auto_field()
+
+
+class SignInSchema(SQLAlchemySchema):
+    class Meta:
+        model = UserModel
+        load_instance = True
+        transient = True
+        unknown = EXCLUDE
+
+    username = auto_field()
+    password = auto_field()
--- tests/unit/interfaces/http/tornado/schemas/v1/test_user_schema.py
+++ tests/unit/interfaces/http/tornado/schemas/v1/test_user_schema.py
@@ -4,7 +4,7 @@ from typing import Any, Dict, List
 import pytest
 from domain.user_model import UserModel
 from infra.constants._string import FieldNameConstants
-from interfaces.http.tornado.schemas.v1.user_schema import SignUpSchema
+from interfaces.http.tornado.schemas.v1.user_schema import SignInSchema, SignUpSchema
 from marshmallow.exceptions import ValidationError
 
 from tests.utils.base_tests import BaseUnitTest
@@ -95,4 +95,52 @@ class TestSignUpSchema(BaseUnitTest):
         assert isinstance(sut.password, str)
         assert isinstance(sut.full_name, str)
         assert isinstance(sut.date_of_birth, date)
+        assert sut.email is None
+
+
+class TestSignInSchema(BaseUnitTest):
+
+    def _create_records(self, size: int) -> List[Dict[Any, Any]]:
+        return UserSeed.create_seed(size=size)
+
+    def test_should_return_an_instance_properly(self):
+        # Arrange
+        # Act
+        sut = SignInSchema()
+        # Assert
+        assert sut is not None
+        assert isinstance(sut, SignInSchema)
+
+    def test_should_raise_validation_error(self):
+        with pytest.raises(ValidationError) as validation_err:
+            # Arrange
+            # Act
+            SignInSchema().load(data=dict())
+            # Assert
+            assert validation_err.type is ValidationError
+
+    def test_should_load_only_required_and_return_properly(self):
+        # Arrange
+        size = 1
+        seed_data = self._create_records(size)[0]
+        # Act
+        sut = SignInSchema().load(data=seed_data)
+        # Assert
+        assert sut is not None
+        assert isinstance(sut, UserModel)
+        assert hasattr(sut, FieldNameConstants.CREATED_AT)
+        assert hasattr(sut, FieldNameConstants.UPDATED_AT)
+        assert hasattr(sut, FieldNameConstants.OBJECT_ID)
+        assert hasattr(sut, FieldNameConstants.USERNAME)
+        assert hasattr(sut, FieldNameConstants.PASSWORD)
+        assert hasattr(sut, FieldNameConstants.FULL_NAME)
+        assert hasattr(sut, FieldNameConstants.DATE_OF_BIRTH)
+        assert hasattr(sut, FieldNameConstants.EMAIL)
+        assert sut.created_at is None
+        assert sut.updated_at is None
+        assert sut._id is None
+        assert isinstance(sut.username, str)
+        assert isinstance(sut.password, str)
+        assert sut.full_name is None
+        assert sut.date_of_birth is None
         assert sut.email is None
