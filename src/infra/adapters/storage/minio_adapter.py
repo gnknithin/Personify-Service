da29ee779778607ceae83409eeda3bd90ee3c841
Nithin <gnknithin@gmail.com>
2024-02-18 12:46:02 +0530

    feat (core) : modified MinioAdapter

--- src/infra/adapters/storage/minio_adapter.py
+++ src/infra/adapters/storage/minio_adapter.py
@@ -1,28 +1,43 @@
 from logging import Logger
-from typing import Type
+from typing import Optional
 
-from minio import Minio
-
-from .base_storage_adapter import BaseStorageAdapter
+from infra.adapters.storage.base_storage_adapter import BaseStorageAdapter
+from minio.api import Minio
+from minio.lifecycleconfig import LifecycleConfig
 
 
 class MinioAdapter(BaseStorageAdapter):
     def __init__(
-        self, logger: Logger, host: str, access_key: str, secret_key: str
+        self,
+        logger: Logger,
+        host: str,
+        access_key: str,
+        secret_key: str,
+        secure: bool,
     ) -> None:
         super().__init__(logger)
-        self._client: Type[Minio] = Minio(
-            endpoint=host,
-            access_key=access_key,
-            secret_key=secret_key
+        self._client = Minio(
+            endpoint=host, access_key=access_key, secret_key=secret_key, secure=secure
         )
 
-    def check_availability(self) -> bool:
-        """
-        Check availability
+    @property
+    def client(self) -> Minio:
+        return self._client
 
-        # Return
+    def bucket_exists(self, name: str) -> bool:
+        return self.client.bucket_exists(bucket_name=name)
 
-        bool
-        """
-        raise NotImplementedError
+    def create_bucket(self, name: str):
+        return self.client.make_bucket(bucket_name=name)
+
+    def get_bucket_lifecycle(self, name: str) -> Optional[LifecycleConfig]:
+        return self.client.get_bucket_lifecycle(bucket_name=name)
+
+    def get_bucket_policy(self, name: str):
+        return self.client.get_bucket_policy(bucket_name=name)
+
+    def list_buckets(self):
+        return self.client.list_buckets()
+
+    def delete_bucket(self, name: str):
+        return self.client.remove_bucket(bucket_name=name)
