973bee6fde9805c45c25abafa11345c0b3facfec
Nithin <gnknithin@gmail.com>
2024-03-16 11:45:25 +0530

    feat (core) : removed Unwanted code from MinIOAppUserAdapter

--- src/infra/adapters/storage/minio_app_user_adapter.py
+++ src/infra/adapters/storage/minio_app_user_adapter.py
@@ -22,23 +22,5 @@ class MinIOAppUserAdapter(BaseStorageAdapter):
     def client(self) -> Minio:
         return self._client
 
-    # def bucket_exists(self, bucket_name: str) -> bool:
-    #     return self.client.bucket_exists(bucket_name=bucket_name)
-
-    # def create_bucket(self, bucket_name: str) -> None:
-    #     return self.client.make_bucket(bucket_name=bucket_name)
-
-    # def get_bucket_lifecycle(self, bucket_name: str) -> Optional[LifecycleConfig]:
-    #     return self.client.get_bucket_lifecycle(bucket_name=bucket_name)
-
-    # def get_bucket_policy(self, bucket_name: str) -> str:
-    #     return self.client.get_bucket_policy(bucket_name=bucket_name)
-
-    # def list_buckets(self) -> List[Bucket]:
-    #     return self.client.list_buckets()
-
-    # def delete_bucket(self, bucket_name: str) -> None:
-    #     return self.client.remove_bucket(bucket_name=bucket_name)
-
     # def create_object(self, bucket_name: str, object_name: str, data: str):
     #     return self.client.put_object()
